1b721d3c513cc31e398bd3e8b7f5f836b1dbd8c8
iluiscastillo <luisedgastillog@gmail.com>
2018-03-24 00:20:15 -0400

    This just the easy part...

---
+++ web/scripts/main.js
@@ -0,0 +1,46 @@
+$(document).ready(() => {
+    console.log("THE DOC IS READY :)");
+    drawing();
+});
+
+const drawing = () => {
+    var myCanvas = document.getElementById("myCanvas");
+    var ctx = myCanvas.getContext("2d");
+    setProps(myCanvas, 800, 500);
+    let dimensionProps = {left: myCanvas.offsetLeft, top: myCanvas.offsetTop};
+    if (myCanvas) {
+        let mouseisDown = false;
+        let cPosX, cPosY;
+        ctx.lineWidth = 5;
+        $(myCanvas).mousedown((e) => {
+            mouseisDown = true;
+            ctx.beginPath();
+            cPosX = e.pageX - dimensionProps.left;
+            cPosY = e.pageY - dimensionProps.top;
+            ctx.moveTo(cPosX, cPosY);
+        }).mousemove((e) => {
+            if (mouseisDown != false) {
+                cPosX = e.pageX - dimensionProps.left;
+                cPosY = e.pageY - dimensionProps.top;
+                ctx.lineTo(cPosX, cPosY);
+                ctx.strokeStyle = "black";
+                ctx.stroke();
+            }
+        }).mouseup((e) => {
+            mouseisDown = false;
+            ctx.closePath();
+        });
+    }
+    clear(ctx, myCanvas);
+};
+
+const setProps = (canvas, w, h) => {
+    canvas.width = w;
+    canvas.height = h;
+}
+
+const clear = (ctx, canvas) => {
+    $("#clear-btn").click(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    });
+}
